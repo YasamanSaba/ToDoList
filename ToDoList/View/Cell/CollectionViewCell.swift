8e8d70aec3c8751f1158b787c782c13273b0fa44
Yasaman <yasaman_saba@yahoo.com>
2020-04-19 15:34:26 +0430

    Add and implement some properties and methods in CollectionViewCell class.

--- ToDoList/View/Cell/CollectionViewCell.swift
+++ ToDoList/View/Cell/CollectionViewCell.swift
@@ -15,6 +15,14 @@ class CollectionViewCell: UICollectionViewCell {
     private var goalView: UIView!
     private var goalViewCreate: UIView!
     private var createDayBtn: UIButton!
+    private var goalViewShow: UIView!
+    private var goalTimeLbl: UILabel!
+    private var goalEdit: UIImageView!
+    private var hoursGoalLbl: UILabel!
+    private var goalViewDatePicker: UIView!
+    private var datePicker: UIDatePicker!
+    private var datePickerTxt: UITextField!
+    private var toolbarDatePicker : UIToolbar!
     private var timerImage: UIImageView!
     private var progressLbl: UILabel!
     private var percentageLbl: UILabel!
@@ -24,7 +32,73 @@ class CollectionViewCell: UICollectionViewCell {
     private var day: Day!
     private var dayService: DayServiceType!
     
+    private var mainStack: UIStackView!
+    
     // MARK: - Functions
+    
+    fileprivate func applyConstraints() {
+        NSLayoutConstraint.activate([
+            
+            self.goalEdit.trailingAnchor.constraint(equalTo: self.goalViewShow.trailingAnchor, constant: -2),
+            self.goalEdit.topAnchor.constraint(equalTo: self.goalViewShow.topAnchor),
+            
+            self.goalTimeLbl.leadingAnchor.constraint(equalTo: self.goalViewShow.leadingAnchor, constant: 5),
+            self.goalTimeLbl.topAnchor.constraint(equalTo: self.goalViewShow.topAnchor, constant: 3),
+            
+            self.hoursGoalLbl.leadingAnchor.constraint(equalTo: self.goalViewShow.leadingAnchor, constant: 10),
+            self.hoursGoalLbl.topAnchor.constraint(equalTo: self.goalTimeLbl.bottomAnchor, constant: 2),
+            
+            self.goalView.widthAnchor.constraint(equalTo: self.contentView.widthAnchor, multiplier: 3 / 4),
+            self.goalView.heightAnchor.constraint(equalTo: self.contentView.heightAnchor, multiplier: 1 / 3),
+            self.goalView.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor),
+            self.goalView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            
+            self.goalViewCreate.widthAnchor.constraint(equalTo: self.goalView.widthAnchor),
+            self.goalViewCreate.heightAnchor.constraint(equalTo: self.goalView.heightAnchor),
+            self.goalViewCreate.centerXAnchor.constraint(equalTo: self.goalView.centerXAnchor),
+            self.goalViewCreate.centerYAnchor.constraint(equalTo: self.goalView.centerYAnchor),
+            
+            self.createDayBtn.centerXAnchor.constraint(equalTo: self.goalViewCreate.centerXAnchor),
+            self.createDayBtn.centerYAnchor.constraint(equalTo: self.goalViewCreate.centerYAnchor),
+            
+            self.goalViewShow.widthAnchor.constraint(equalTo: self.goalView.widthAnchor),
+            self.goalViewShow.heightAnchor.constraint(equalTo: self.goalView.heightAnchor),
+            self.goalViewShow.centerXAnchor.constraint(equalTo: self.goalView.centerXAnchor),
+            self.goalViewShow.centerYAnchor.constraint(equalTo: self.goalView.centerYAnchor),
+            
+            self.goalViewDatePicker.widthAnchor.constraint(equalTo: self.goalView.widthAnchor),
+            self.goalViewDatePicker.heightAnchor.constraint(equalTo: self.goalView.heightAnchor),
+            self.goalViewDatePicker.centerXAnchor.constraint(equalTo: self.goalView.centerXAnchor),
+            self.goalViewDatePicker.centerYAnchor.constraint(equalTo: self.goalView.centerYAnchor),
+            
+            self.datePickerTxt.leadingAnchor.constraint(equalTo: self.goalViewDatePicker.leadingAnchor),
+            self.datePickerTxt.trailingAnchor.constraint(equalTo: self.goalViewDatePicker.trailingAnchor),
+            self.datePickerTxt.centerXAnchor.constraint(equalTo: self.goalViewDatePicker.centerXAnchor),
+            self.datePickerTxt.centerYAnchor.constraint(equalTo: self.goalViewDatePicker.centerYAnchor),
+            
+            self.binBtn.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
+            self.binBtn.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
+            
+            self.mainStack.bottomAnchor.constraint(equalTo: self.binBtn.topAnchor, constant: -5),
+            self.mainStack.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 10),
+            self.mainStack.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -10),
+            self.mainStack.topAnchor.constraint(equalTo: self.goalView.bottomAnchor, constant: 15)
+        ])
+    }
+    
+    fileprivate func addSubviews() {
+        self.goalView.addSubview(self.goalViewCreate)
+        self.goalView.addSubview(self.goalViewShow)
+        self.goalView.addSubview(self.goalViewDatePicker)
+        self.goalViewCreate.addSubview(self.createDayBtn)
+        self.goalViewShow.addSubview(self.goalEdit)
+        self.goalViewShow.addSubview(self.goalTimeLbl)
+        self.goalViewShow.addSubview(self.hoursGoalLbl)
+        self.goalViewDatePicker.addSubview(self.datePickerTxt)
+        self.contentView.addSubview(self.goalView)
+        self.contentView.addSubview(self.binBtn)
+        self.contentView.addSubview(self.mainStack)
+    }
     
     /// It puts UI elements in the `CollectionViewCell`
     func configure() {
@@ -40,9 +114,51 @@ class CollectionViewCell: UICollectionViewCell {
         self.dateLbl.translatesAutoresizingMaskIntoConstraints = false
         
         self.goalView = UIView()
+        //self.goalView.backgroundColor = .systemBlue
         self.goalView.translatesAutoresizingMaskIntoConstraints = false
         
-        self.goalViewCreate = UIView(frame: CGRect(x: 0, y: 0, width: self.contentView.bounds.width * 2 / 3, height: self.contentView.bounds.height / 5))
+        self.goalViewCreate = UIView()
+        self.goalViewCreate.translatesAutoresizingMaskIntoConstraints = false
+        self.goalViewCreate.isHidden = true
+        
+        self.goalViewShow = UIView()
+        //self.goalViewShow.backgroundColor = .systemRed
+        self.goalViewShow.layer.borderWidth = 1.0
+        self.goalViewShow.layer.borderColor = UIColor.systemGray5.cgColor
+        self.goalViewShow.translatesAutoresizingMaskIntoConstraints = false
+        self.goalViewShow.isHidden = true
+        
+        self.goalViewDatePicker = UIView()
+        self.goalViewDatePicker.translatesAutoresizingMaskIntoConstraints = false
+        //self.goalViewDatePicker.isHidden = true
+        
+        self.datePicker = UIDatePicker()
+        self.datePicker.datePickerMode = .countDownTimer
+        self.datePicker.addTarget(self, action: #selector(handler), for: UIControl.Event.valueChanged)
+        
+        self.datePickerTxt = UITextField()
+        self.datePickerTxt.placeholder = "Add your goal time "
+        self.datePickerTxt.borderStyle = .roundedRect
+        self.datePickerTxt.inputView = self.datePicker
+        self.datePickerTxt.tintColor = .clear
+        self.datePickerTxt.translatesAutoresizingMaskIntoConstraints = false
+        
+        // ToolBar
+        self.toolbarDatePicker = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.contentView.bounds.width, height: 44))
+        self.toolbarDatePicker.barStyle = UIBarStyle.default
+        self.toolbarDatePicker.isTranslucent = true
+        self.toolbarDatePicker.tintColor = UIColor(red: 92/255, green: 216/255, blue: 255/255, alpha: 1)
+        self.toolbarDatePicker.sizeToFit()
+        
+        
+        // Adding Button ToolBar
+         let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneClick))
+         let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+         let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelClick))
+         toolbarDatePicker.setItems([cancelButton, spaceButton, doneButton], animated: false)
+         toolbarDatePicker.isUserInteractionEnabled = true
+         datePickerTxt.inputAccessoryView = toolbarDatePicker
+        
         
         self.createDayBtn = UIButton()
         self.createDayBtn.setTitle("+ Create your day", for: .normal)
@@ -50,6 +166,24 @@ class CollectionViewCell: UICollectionViewCell {
         self.createDayBtn.titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
         self.createDayBtn.addTarget(self, action: #selector(createDay), for: .touchUpInside)
         self.createDayBtn.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.goalTimeLbl = UILabel()
+        self.goalTimeLbl.text = "Goal is:"
+        self.goalTimeLbl.font = UIFont.preferredFont(forTextStyle: .title3)
+        self.goalTimeLbl.textColor = .label
+        self.goalTimeLbl.minimumScaleFactor = 0.4
+        self.goalTimeLbl.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.goalEdit = UIImageView()
+        self.goalEdit.image = UIImage(systemName: "pencil")
+        self.goalEdit.contentMode = .scaleToFill
+        self.goalEdit.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.hoursGoalLbl = UILabel()
+        self.hoursGoalLbl.text = "2 hour 45 min"
+        self.hoursGoalLbl.font = UIFont.preferredFont(forTextStyle: .body)
+        self.hoursGoalLbl.minimumScaleFactor = 0.4
+        self.hoursGoalLbl.translatesAutoresizingMaskIntoConstraints = false
         
         self.timerImage = UIImageView()
         self.timerImage.image = UIImage(systemName: "timer")
@@ -93,36 +227,25 @@ class CollectionViewCell: UICollectionViewCell {
         progressStack.alignment = .leading
         progressStack.translatesAutoresizingMaskIntoConstraints = false
         
-        let mainStack = UIStackView(arrangedSubviews: [self.dateLbl, progressStack])
+        self.mainStack = UIStackView(arrangedSubviews: [self.dateLbl, progressStack])
         mainStack.axis = .vertical
         mainStack.distribution = .fill
         mainStack.spacing = 7.0
         mainStack.alignment = .center
         mainStack.translatesAutoresizingMaskIntoConstraints = false
         
-        self.goalViewCreate.addSubview(self.createDayBtn)
-        self.goalView.addSubview(self.goalViewCreate)
-        self.contentView.addSubview(self.goalView)
-        self.contentView.addSubview(self.binBtn)
-        self.contentView.addSubview(mainStack)
-        NSLayoutConstraint.activate([
-            self.createDayBtn.centerXAnchor.constraint(equalTo: self.goalView.centerXAnchor),
-            self.createDayBtn.centerYAnchor.constraint(equalTo: self.goalView.centerYAnchor),
-            self.goalView.widthAnchor.constraint(equalTo: self.contentView.widthAnchor, multiplier: 3 / 4),
-            self.goalView.heightAnchor.constraint(equalTo: self.contentView.heightAnchor, multiplier: 1 / 3),
-            self.goalView.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor),
-            self.goalView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
-            self.binBtn.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
-            self.binBtn.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
-            mainStack.bottomAnchor.constraint(equalTo: self.binBtn.topAnchor, constant: -5),
-            mainStack.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 10),
-            mainStack.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -10),
-            mainStack.topAnchor.constraint(equalTo: self.goalView.bottomAnchor, constant: 15)
-        ])
+        addSubviews()
+        
+        applyConstraints()
     }
     
     @objc func createDay() {
-
+        UIView.transition(from: self.goalViewCreate,
+                          to: self.goalViewDatePicker,
+                          duration: 0.5,
+                          options: [.transitionFlipFromBottom, .showHideTransitionViews]) { _ in
+                         self.datePickerTxt.becomeFirstResponder()
+        }
     }
     
     @objc func deleteDay() {
@@ -131,5 +254,28 @@ class CollectionViewCell: UICollectionViewCell {
     
     @objc func addTask() {
         
+    }
+    
+    @objc func doneClick() {
+        
+    }
+    
+    @objc func cancelClick() {
+        self.datePickerTxt.resignFirstResponder()
+        UIView.transition(from: self.goalViewDatePicker,
+                          to: self.goalViewCreate,
+                          duration: 0.5,
+                          options: [.transitionFlipFromBottom, .showHideTransitionViews],
+                          completion: nil)
+    }
+    
+    @objc func handler(sender: UIDatePicker) {
+        let timeFormatter = DateFormatter()
+        timeFormatter.dateFormat = "HH:mm"
+        let strDate = timeFormatter.string(from: datePicker.date)
+        self.datePickerTxt.text = strDate
+        self.datePickerTxt.textAlignment = .center
+        self.datePickerTxt.font = UIFont.preferredFont(forTextStyle: .title1)
+        self.datePickerTxt.borderStyle = .roundedRect
     }
 }
